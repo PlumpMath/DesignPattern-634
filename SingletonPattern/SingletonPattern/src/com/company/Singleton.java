ba0ebb1918182bcfdbb2ee58d898a9fcf3127e45
Nightwingky <qky1024@163.com>
2016-09-14 15:03:28 +0800

    添加“饿汉”单例模式

---
+++ SingletonPattern/SingletonPattern/src/com/company/Singleton.java
@@ -0,0 +1,28 @@
+package com.company;
+
+/**
+ * Created by nightwingky on 16-9-14.
+ */
+public class Singleton {
+
+    //构造方法私有化
+    private Singleton() {
+
+    }
+
+    //创建类的唯一实例
+    private static Singleton instance = new Singleton();
+
+    //提供用于获取实例的方法
+    public static Singleton getInstance() {
+        return instance;
+    }
+
+    public static void setInstance(Singleton instance) {
+        Singleton.instance = instance;
+    }
+
+    public void print() {
+        System.out.println("Singleton");
+    }
+}
